f8d3a24d9b0d2b230c82ba5cc39d2f71addc9172
saif <mdsaif5893@gmail.com>
2024-03-06 20:17:50 +0530

    fixed issue

--- left_rotaion.py
+++ left_rotaion.py
@@ -1,4 +1,14 @@
 def left_rotation(num, d):
+    """
+    The function `left_rotation` performs a left rotation on a list of numbers by a specified amount.
+    
+    :param num: [1, 2, 3, 4, 5, 5, 6, 7, 8]
+    :param d: The variable `d` represents the number of positions by which the elements in the list
+    `num` should be left rotated. In this case, `d` is set to 1, so the elements in the list `num` will
+    be rotated to the left by 1 position
+    :return: The function `left_rotation` takes a list of numbers `num` and an integer `d` as input. It
+    performs a left rotation on the list `num` by `d` positions and returns the rotated list.
+    """
     n = len(num)
     return num[d % n:] + num[:d % n]
 
